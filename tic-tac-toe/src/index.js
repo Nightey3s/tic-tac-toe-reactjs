cd57418dcf64d762125afe43a040a434d997673b
Brian Tham <briantham1@gmail.com>
2020-10-06 10:37:14 +0800

    Tutorial Step: Declaring a winner | Game complete
    - Added a check to see if there is a winner
    - Ignore clicks if winner is present

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -56,6 +56,9 @@ class Board extends React.Component {
     
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if(calculateWinner(squares) || squares[i]){
+            return; //this will exit and ignore clicks if there is a winner
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares:squares,
@@ -71,7 +74,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);   //check for winner
+      let status;
+      if(winner){//if winner, change text to display winner
+          status = 'Winner: ' + winner;
+      }
+      else {//else continue game
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
 
     return (
       <div>
@@ -112,6 +122,27 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+/* Given an array of 9 squares, this function will check for a winner and return 'X', 'O', or null as appropriate. */
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
